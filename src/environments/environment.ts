922c0025c87b15a84e6114c1de8944d0e68a3447
Peter Ajayi <peterdgreat2016@gmail.com>
2024-10-03 23:50:28 +0100

    removed api/v1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1/', // Use your local API URL for development
+  apiUrl: 'http://localhost:3000', // Use your local API URL for development
 };
